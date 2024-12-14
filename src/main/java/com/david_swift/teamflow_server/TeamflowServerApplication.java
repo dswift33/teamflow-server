644c446470d810e699447809f2fb847a1a27b4e3
davidaswift <98354056+davidaswift@users.noreply.github.com>
2024-12-13 20:13:18 -0500

    Initial spring boot project

---
+++ src/main/java/com/david_swift/teamflow_server/TeamflowServerApplication.java
@@ -0,0 +1,13 @@
+package com.david_swift.teamflow_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamflowServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamflowServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/david_swift/teamflow_server/TeamflowServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david_swift.teamflow_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamflowServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
