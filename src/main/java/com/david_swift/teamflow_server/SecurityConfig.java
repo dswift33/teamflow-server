4c0219394a9e61741ba86fd5d58a3b7f5dac982a
davidaswift <98354056+davidaswift@users.noreply.github.com>
2024-12-14 07:42:39 -0500

    Initial spring boot project

--- src/main/java/com/david_swift/teamflow_server/SecurityConfig.java
+++ src/main/java/com/david_swift/teamflow_server/SecurityConfig.java
@@ -4,14 +4,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-//    @Bean
-//    HttpSecurity httpSecurity(HttpSecurity http) throws Exception {
-//        http.csrf().disable()
-//                .authorizeRequests()
-//                .mvcMatchers("/api/auth/**").permitAll()
-//                .anyRequest().authenticated();
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authorize) -> authorize.requestMatchers("/api/**")
+                        .authenticated()
+                        .anyRequest()
+                        .permitAll())
+                .formLogin(withDefaults());
+        return http.build();
     }
+}
